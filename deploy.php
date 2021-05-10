4760eff0a2e992768be15f88d5a09c90df20cf86
IlanRubinsohn <ilanrubinsohn13@gmail.com>
2021-05-10 14:01:21 +0200

    mise a jour deploy v2

---
+++ deploy.php
@@ -0,0 +1,43 @@
+<?php
+namespace Deployer;
+
+require 'recipe/symfony.php';
+
+// Project name
+set('application', 'API_REST_ILAN');
+
+// Project repository
+set('repository', 'git@github.com:IlanRubinsohn/API_REST_ILAN.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+// Hosts
+
+host('20.188.34.16')
+    ->user('azureuser')
+    ->addSshOption('UserKnownHostsFile', '/dev/null')
+    ->addSshOption('StrictHostKeyChecking', 'no')
+    ->set('deploy_path', '~/{{application}}');    
+    
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'database:migrate');
+
